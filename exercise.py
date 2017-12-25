40f5efcc69137cc91ddfe4fe6fba9e587688f2c2
chunminggg <651403538@qq.com>
2017-12-25 21:11:26 +0800

    写了几道题  https://github.com/Show-Me-the-Code/show-me-the-code 007

--- exercise.py
+++ exercise.py
@@ -1,16 +1,35 @@
-#字符串内容
-import string
-chars = string.ascii_letters + string.digits
-#返回abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789
-import random
-def getrandom():  #定义一个函数，将一个含有4个随机元素的列表内的元素合并在一起组成一个字符串
-    return "".join(random.sample(chars, 4)) 
+import os, re
 
-def concatenate(m): #定义一个函数，将m个4元素字符串用“-”连接起来组成一个激活码
-    return "-".join([getrandom() for i in range(m)])
+FILE_PATH = ''
 
-def generate(n): #定义一个函数，返回n个激活码组成的列表
-    return [concatenate(4) for i in range(n)]     
-if __name__ == '__main__':
-    for item in generate(10): #把列表里的10个激活码打印出来
-       
+def analyze_code(codefilesource):
+    total_line = 0
+    comment_line = 0
+    blank_line = 0
+    with open(codefilesource) as f:
+        lines = f.readlines()
+        total_line = len(lines)
+        line_index = 0
+        while line_index < total_line:
+            line = lines[line_index]
+            if line.startswith('#'):
+                comment_line += 1
+            elif re.match("\s*'''",line) is not None:
+                comment_line += 1
+                while re.match(".*'''$",line) is None:
+                    line = lines[line_index]
+                    comment_line += 1
+                    line_index += 1
+            elif line == "\n":
+                blank_line += 1
+            line_index += 1
+    return [total_line,comment_line,blank_line]
+def run(filepath):
+    os.chdir(filepath)
+    total_lines = 0
+    total_comment_lines = 0
+    total_blank_lines = 0
+    for i in os.listdir(os.getcwd()):
+        if os.path.splitext(i)[1] == '.py':
+            line = analyze_code(i)
+            total_lines, total_comment_lines, total_blank_lines = total_lines + line[0], total_comment_lines + line[1], total_blank_lines + line[2]
