284673b5ef6e322f4cd86d5f6e6fdfd3696c8c3e
neo-mac <miaobingliang@neotrident.com>
2018-02-13 16:23:42 +0800

    done

--- exercise.py
+++ exercise.py
@@ -1,35 +1,5 @@
-import os, re
-
-FILE_PATH = ''
-
-def analyze_code(codefilesource):
-    total_line = 0
-    comment_line = 0
-    blank_line = 0
-    with open(codefilesource) as f:
-        lines = f.readlines()
-        total_line = len(lines)
-        line_index = 0
-        while line_index < total_line:
-            line = lines[line_index]
-            if line.startswith('#'):
-                comment_line += 1
-            elif re.match("\s*'''",line) is not None:
-                comment_line += 1
-                while re.match(".*'''$",line) is None:
-                    line = lines[line_index]
-                    comment_line += 1
-                    line_index += 1
-            elif line == "\n":
-                blank_line += 1
-            line_index += 1
-    return [total_line,comment_line,blank_line]
-def run(filepath):
-    os.chdir(filepath)
-    total_lines = 0
-    total_comment_lines = 0
-    total_blank_lines = 0
-    for i in os.listdir(os.getcwd()):
-        if os.path.splitext(i)[1] == '.py':
-            line = analyze_code(i)
-            total_lines, total_comment_lines, total_blank_lines = total_lines + line[0], total_comment_lines + line[1], total_blank_lines + line[2]
+from PIL import Image
+import os
+img = Image.open('launchImage.jpg')
+new_img = img.resize((750,1334),Image.ANTIALIAS)
+new_img.save('example-new.jpg',quality=100)
